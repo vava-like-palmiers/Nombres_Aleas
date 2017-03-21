411a7a5815cfa334f16d1728e1a91119f9d144a3
dalessandro <vive-les-palmiersx2@live.fr>
2017-03-21 17:38:41 +0100

    ajout fonction decalage (non testée)

--- e0.c
+++ e0.c
@@ -25,4 +25,40 @@ void set(byte_t * tab, size_t size, size_t i, bool val){
 	tab[caseTab] = normalise | masque;
 }
 
+void decalageLFSR(byte_t * tab, size_t size){
+	bool bit;
+	int i;
+	for(i=0; i<size; i++){
+		bit = get(tab, size, 8*i+7);
+		tab[i] = tab[i] >> 1;
+		if(i != 0){
+			set(tab, size, 8*i, bit);
+		}
+	}
+}
 
+bool premierBit(bool bit1, bool bit2, bool bit3, bool bit4){
+    return bitASet = bit1 ^ bit2 ^ bit3 ^ bit4;
+}
+
+void LFSR(size_t size, bool bit1, bool bit2, bool bit3, bool bit4){
+    int tab[size/8+1];
+    int i;
+    bool bit;
+    tabToString(tab, size);
+    for(int=0; i<5; i++){
+        bit = premierBit(bit1, bit2, bit3, bit4);
+        decalageLFSR(tab, size);
+        set(tab, size, 0, bit);
+        tabToString(tab, size);
+    }
+}
+
+void tabToString(byte_t * tab, size_t size){
+    int i;
+    printf{"{"};
+    for(i=0; i<size; i++){
+        printf(" %d ", get(tab, size, i));
+    }
+    printf{"}\n"};
+}
